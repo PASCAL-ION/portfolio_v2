f194a54e5925c4d1dd0d975bd1444f73596a78ef
pascal ion <ion.pascal@epitech.eu>
2025-03-30 21:12:50 +0200

    ADD: lenis for smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useLocation, Routes, Route } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
+import Lenis from "@studio-freight/lenis"
 import {
   Contact,
   Home,
@@ -10,12 +11,14 @@ import {
   ProjectDetail
 } from "./pages/index.js";
 import { Layout } from "./components/Layout";
-import {Loader} from "./components/Loader";
+import { Loader } from "./components/Loader";
+
 
 function App() {
   const location = useLocation();
   const [background, setBackground] = useState("bg-[#0C0C0C]");
   const [loading, setLoading] = useState(false);
+  const lenis = useRef(null);
 
   useEffect(() => {
     const hasVisited = localStorage.getItem("hasVisited");
@@ -32,6 +35,25 @@ function App() {
   useEffect(() => {
     setBackground("bg-[#0C0C0C]");
   }, [location]);
+
+  useEffect(() => {
+    lenis.current = new Lenis({
+      duration: 0.8,
+      easing: (t) => 1 - Math.pow(1 - t, 3),
+      smooth: true,
+      smoothTouch: true,
+    });
+
+    const raf = (time) => {
+      lenis.current.raf(time);
+      requestAnimationFrame(raf);
+    };
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.current.destroy();
+    };
+  }, []);
 
   return (
     <>
@@ -66,3 +88,4 @@ function App() {
 }
 
 export default App;
+
