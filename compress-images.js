a6a90c95045d28d21e72f050a1c7b36eb13be000
pascal ion <ion.pascal@epitech.eu>
2025-04-14 14:50:14 +0200

    ADD: images compress script

---
+++ compress-images.js
@@ -0,0 +1,41 @@
+import imagemin from 'imagemin';
+import mozjpeg from 'imagemin-mozjpeg';
+import pngquant from 'imagemin-pngquant';
+import path from 'path';
+import fs from 'fs/promises';
+
+const sourceDir = 'src/assets';
+
+const getImageFiles = async (dir) => {
+  const entries = await fs.readdir(dir, { withFileTypes: true });
+  const files = await Promise.all(entries.map(async (entry) => {
+    const fullPath = path.join(dir, entry.name);
+    if (entry.isDirectory()) {
+      return await getImageFiles(fullPath);
+    } else if (/\.(jpe?g|png)$/i.test(entry.name)) {
+      return fullPath;
+    }
+    return null;
+  }));
+  return files.flat().filter(Boolean);
+};
+
+const compressImagesInPlace = async () => {
+  const images = await getImageFiles(sourceDir);
+
+  for (const imagePath of images) {
+    const buffer = await imagemin([imagePath], {
+      plugins: [
+        mozjpeg({ quality: 70 }),
+        pngquant({ quality: [0.6, 0.8] })
+      ]
+    });
+
+    if (buffer[0]) {
+      await fs.writeFile(imagePath, buffer[0].data);
+      console.log(`Image compressée : ${imagePath}`);
+    }
+  }
+};
+
+compressImagesInPlace();
